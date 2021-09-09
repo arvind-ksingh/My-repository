0af70f46a0cb42e3f1f300223a5cf6f06560ddf9
arvind-ksingh <82834110+arvind-ksingh@users.noreply.github.com>
2021-09-09 15:10:44 +0530

    Add files via upload
    first upload

---
+++ 2.3.java
@@ -0,0 +1,24 @@
+class Hello{
+int a=10;
+int b;
+int Hello;
+Hello h1;
+void m1(){
+int ab=100;
+int bc;
+int a=101;
+int b;
+System.out.println("m1 in Hello");
+System.out.println(a);
+System.out.println(b);
+System.out.println(Hello);
+System.out.println(h1);
+System.out.println(ab);
+System.out.println(bc);
+}}
+public class Jtc7{
+public static void main(String[] arg){
+Hello h1=new Hello();
+h1.m1();
+}
+}
